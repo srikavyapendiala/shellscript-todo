42b2690cb22ec1f70dce68f3f0a174cddfff76f2
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-04 12:34:21 +0530

    #1 create component files & common.sh file

--- todo/components/common.sh
+++ todo/components/common.sh
@@ -1,14 +1,53 @@
 #!/bin/bash
 
 HEAD(){
-  echo -e "\e[1;36m===============$1\e[0m"
-  echo -e "\e[1;36m===============$1\e[0m" >>$LOG
+  echo -e "\e[1;36m =============================$1\e[0m"
+  echo -e "\e[1;36m =============================$1\e[0m" >>"${LOG}"
 }
-UPDATE() {
-  set-hostname $"{COMPONENT}"
-  apt update
+STAT(){
+  if [ "${1}" -eq 0 ]; then
+    echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILURE\e[0m"
+    echo  -e "\e[1m;31m Pls refer log file for more information, log filepath is =${LOG}\e[0m"
+  fi
 }
-
+NPM(){
+  apt install npm -y
+}
+GIT_CLONE(){
+  git clone "https://github.com/zelar-soft-todoapp/${COMPONENT}.git"
+  cd "${COMPONENT}" || exit
+}
+REPEAT(){
+  set-hostname "${COMPONENT}"
+  HEAD "Updating apt repos"
+  apt update >>"${LOG}"
+}
+NPM_INSTALL(){
+  npm install
+}
 ERROR(){
   echo -e "\e[1;31m$1\e[0m"
 }
+#main.sh
+#!/bin/bash
+COMPONENT=$1
+export LOG=/tmp/"${COMPONENT}".log
+rm -rf "$(LOG)"
+source components/common.sh
+if [ ! -f components/"${COMPONENT}".sh ]; then
+  ERROR "Invalid File"
+  exit 1
+fi
+USER_NAME=$(whoami)
+if [ "${USER_NAME}" != "root" ]; then
+  ERROR "You should be a root user to execute these scripts"
+  exit 1
+fi
+export COMPONENT
+bash components/"${COMPONENT}".sh
+Collapse
+
+
+
--- todo/components/frontend.sh
+++ todo/components/frontend.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
 source components/common.sh
+HEAD "Set hostname & update repo"
+REPEAT
 
-OS_PREREQ
+HEAD "Install Nginx"
+apt install nginx -y
+
+HEAD "Start Nginx"
+systemctl start nginx
+
+HEAD "Install Node & Nginx"
+NPM
+STAT $?
+
+HEAD "Change directory and make todo directory and switch to todo directory"
+cd /var/www/html && mkdir "todo" && cd todo || exit
+STAT $?
+
+HEAD "Clone code from Github"
+GIT_CLONE
+STAT $?
+
+HEAD "Install Npm"
+NPM_INSTALL
+STAT $?
+
+HEAD "Run build"
+npm run build
+STAT $?
+
+HEAD "Change root path in nginx"
+sed -i -e 's/(/var/www/html)/(/var/www/html/todo/frontend/dist)' /etc/nginx/sites-available/default
+STAT $?
+
+HEAD "Update index.js File With Todo & Login Ip"
+cd /var/www/html/todo/frontend && cd config || exit
+vi index.js
+sed -i -e 's/localhost/paymentip'
+STAT $?
+
+HEAD "Restart Nginx"
+systemctl restart nginx
+STAT $?
+
+HEAD "run npm start"
+npm start
+STAT $?
+
