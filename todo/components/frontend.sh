c17eaf8aaef9db5ea65d2fc8bef496c7a2232e9d
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-11 18:28:37 +0530

    #1 create component files & common.sh file

--- todo/components/frontend.sh
+++ todo/components/frontend.sh
@@ -2,59 +2,37 @@
 source components/common.sh
 HEAD "Set hostname & update repo"
 REPEAT
-
 HEAD "Install Nginx"
-apt install nginx -y >>"${LOG}"
-
+apt install nginx -y &>>"${LOG}"
 HEAD "Start Nginx"
 systemctl start nginx
-
 HEAD "Install Node & Nginx"
 NPM
 STAT $?
-
-HEAD "Change directory and make todo directory and switch to todo directory"
+HEAD "switch to html directory "
 cd /var/www/html
-sudo find . -type d -name "todo"
-
-# shellcheck disable=SC2181
-if [ $? -ne 0 ]; then
-  mkdir "todo"
-  STAT $?
-fi
-
-cd todo || exit
 STAT $?
-
+HEAD "switch to vue directory"
+mkdir vue && cd vue
+STAT $?
 HEAD "Clone code from Github"
 GIT_CLONE
 STAT $?
-
+HEAD "Switch to frontend Directory"
+cd frontend
+STAT $?
 HEAD "Install Npm"
 npm install >>"${LOG}"
 STAT $?
-
 HEAD "Run build"
 BUILD
-killall node >>"${LOG}"
-BUILD
 STAT $?
-
 HEAD "Change root path in nginx"
-cd /etc/nginx/sites-available || exit
-
-sed -i 's/\/var\/www\/html/\/var\/www\/html\/todo\/frontend\/dist/g' filename.txt default
+sed -i -e 's+root /var/www/html+root /var/www/html/vue/frontend/dist+g' /etc/nginx/sites-available/default
 STAT $?
-
-HEAD "Update index.js File With Todo & Login Ip"
-cd /var/www/html/todo/frontend && cd config || exit
-vi index.js
-STAT $?
-
 HEAD "Restart Nginx"
 systemctl restart nginx
 STAT $?
-
 HEAD "run npm start"
 npm start
 STAT $?
