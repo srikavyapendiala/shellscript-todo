a013c822846a0c5618a8ba993506c7b2df926643
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-11 19:01:54 +0530

    #1 create component files & common.sh file

--- todo/components/todo.sh
+++ todo/components/todo.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 source components/common.sh
+HEAD "Set hostname and update repo"
 REPEAT
-
+STAT $?
+HEAD "Install npm"
 NPM
+STAT $?
 HEAD "Clone code from github"
 GIT_CLONE
 STAT $?
-
 HEAD "Install npm"
-npm install
+npm install >>"${LOG}"
 STAT $?
-
 HEAD "Create service file"
-vi /etc/systemd/system/todo.service
-STAT $?
-
+mv /root/shellscripting-todo/todo/todo/systemd.service /etc/systemd/system/todo.service
 HEAD "Start Todo Service"
 systemctl daemon-reload && systemctl start todo && systemctl status todo
 STAT $?
-
- HEAD "Start npm"
- npm start
- STAT $?
