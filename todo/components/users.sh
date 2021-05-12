d808f0dd0d7ad9f016f7adf32a2e4e4d9a3a00d9
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-12 11:44:28 +0530

    #1 create component files & common.sh file

--- todo/components/users.sh
+++ todo/components/users.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
 source components/common.sh
-
 HEAD "Set Hostname and Update repo"
 REPEAT
 Stat $?
 
 HEAD "Install java-openjdk"
-apt-get install openjdk-8-jdk-headless -y &>>"$LOG"
+apt-get install openjdk-8-jdk-headless -y &>>${LOG}
 Stat $?
 
 HEAD "Installing Maven"
-apt install maven -y &>>"$LOG"
+apt install maven -y &>>${LOG}
 Stat $?
 
 HEAD "Cloning the repo"
@@ -19,13 +17,12 @@ GIT_CLONE
 Stat $?
 
 HEAD "cleaning the maven package"
-mvn clean package &>>"$LOG"
+mvn clean package &>>${LOG}
 Stat $?
 
 HEAD "Now move the user services"
 mv /root/shellscripting-todo/todo/users/systemd.service /etc/systemd/system/users.service
 Stat $?
-
 
 HEAD "Restart the services"
 systemctl daemon-reload
