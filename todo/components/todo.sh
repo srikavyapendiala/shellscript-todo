e49f3b8a4d7ad4d459362212f0a90ab1349b251a
chandu <chandu@chandra.com>
2021-05-12 20:31:16 +0530

    #1 create component files & common.sh file

--- todo/components/todo.sh
+++ todo/components/todo.sh
@@ -21,6 +21,9 @@ STAT $?
 HEAD "Create service file"
 mv /root/shellscripting-todo/todo/todo/systemd.service /etc/systemd/system/todo.service
 
+HEAD "Replace Ip with DNS Names"
+sed -i -e 's/Environment=REDIS_HOST=172.31.21.148/Environment=REDIS_HOST=redis.kavya.website/g' /etc/systemd/system/todo.service
+
 
 HEAD "Start Todo Service"
 systemctl daemon-reload && systemctl start todo && systemctl status todo
