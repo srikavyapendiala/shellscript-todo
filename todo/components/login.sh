39a38dc655a98bd253120a8807ad861bb2b5d8fd
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-05-13 11:05:50 +0530

    Update login.sh

--- todo/components/login.sh
+++ todo/components/login.sh
@@ -38,7 +38,7 @@ HEAD "Create login service file"
 mv /root/go/src/login/systemd.service /etc/systemd/system/login.service
 
 HEAD "Replace Ip with DNS Names"
-sed -i -e 's/Environment=USERS_API_ADDRESS=http://172.31.27.160:8080/Environment=USERS_API_ADDRESS=users.kavya.website:8080/g' /etc/systemd/system/login.service
+sed -i -e 's/Environment=USERS_API_ADDRESS=http://172.31.17.148:8080/Environment=USERS_API_ADDRESS=users.kavya.website:8080/g' /etc/systemd/system/login.service
 
 HEAD "Start login service"
 HEAD "Start login service"
